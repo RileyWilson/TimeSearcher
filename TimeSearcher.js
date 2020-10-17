31707ca67b75530d945ce1d6dd6b8c750db667da
Riley Wilson <rileywilson@Rileys-MacBook-Pro.local>
2020-10-17 13:05:52 -0700

    updated filez

--- TimeSearcher.js
+++ TimeSearcher.js
@@ -1,14 +1,19 @@
 'use strict';
+import * as lib from 'https://rileywilson.github.io/TimeSearcher/lib/lib.js'
 
-function myFunction() {
-    var data = d3.csv('./lib/data/top_100.csv');
+const myFunction = async(params) => {
 
+    // load in the csv data
+    var data = await d3.csv('https://rileywilson.github.io/TimeSearcher/lib/data/top_100.csv');
+
+
+    var newData = lib.getData();
+    
     for (var i = 0; i < data.length; i++) {
-        console.log(data[i].Name);
-        console.log(data[i].Age);
+        console.log(data[i].person);
     }
 
-    let ol = d3.create('ol');
+    /*let ol = d3.create('ol');
   
 
     var items = ol.selectAll('li').data(data).join(
@@ -17,20 +22,15 @@ function myFunction() {
         exit   => exit.remove() )
             .text(d => `${d.person}`);
     
-    /*ol.selectAll('li') // select all list elements (orange circle above)
+    ol.selectAll('li') // select all list elements (orange circle above)
         .data(data)  // bind all our data values (blue circle above)
         .join(
           enter  => enter.append('li'), // append an li element for each entering item
           update => update,             // do nothing with items that match an existing element
           exit   => exit.remove()       // remove li elements whose backing data is now gone
         ).text(d => `${d.person}`);
-    //return ol.node(); */
     
-
-    var bitch = d3.select("bitch");
-    bitch.style("color", "green");
-    console.log("hey bitch");
-    document.getElementById("bitch").textContent = "U suk";
+     return ol.node();*/
 }
 
 myFunction();
